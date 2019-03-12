6851e65899751356aa0a4c752ff01cf65f7df2b6
Daniel Nemergut <dnemergut@stsci.edu>
2019-03-12 11:01:41 -0400

    Added plotting function to main.py

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ if __name__ == '__main__':
     print(data[100, 100])
 
     # You can also plot the result:
-    #import matplotlib.pyplot as plt
-    #plt.imshow(data.T, cmap=plt.cm.viridis)
-    #plt.colorbar()
-    #plt.show()
+    import matplotlib.pyplot as plt
+    plt.imshow(data.T, cmap=plt.cm.viridis)
+    plt.colorbar()
+    plt.show()
+
